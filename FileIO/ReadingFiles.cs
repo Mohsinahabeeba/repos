e9a8be090e9da139ef8906e1360cd2f9fdb49030
mohsina habeeba <mohsinahabeeba2@gmail.com>
2018-07-25 17:45:01 +0530

    newreading and writing

---
+++ FileIO/ReadingFiles.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIO
+{
+    class ReadingFiles
+    {
+        public static void ReadForFile(string direc)
+
+        {
+
+
+           // string[] filepath = Directory.GetFiles(direc,"*.txt");
+            DirectoryInfo fi = new DirectoryInfo(direc);
+            FileInfo[] files = fi.GetFiles("*.txt");
+            foreach(FileInfo flp in files)
+            {
+                Console.WriteLine(flp);
+                Console.WriteLine(flp.CreationTime);
+                if(flp.LastWriteTime> DateTime.Now.AddMinutes(-3))
+                {
+                    Console.WriteLine(flp.LastWriteTime);
+                }
+                else
+                {
+                    Console.WriteLine("nothing modified");
+                }
+                
+
+            }
+         }
+
+        public static void Main()
+        {
+            
+            ReadForFile(@"D:\mohs\JSONS\FileIO\data");
+            
+            Console.ReadLine();
+        }
+    }
+}
+
